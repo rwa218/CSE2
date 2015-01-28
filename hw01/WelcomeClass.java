3efdc5d00cbf052a2b32e71560e505248e52ebe4
Riley Amelio <rwa218@lehigh.edu>
2015-01-28 20:39:36 +0000

    finished WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+// Riley Amelio
+// CSE 2
+// HW01
+// Basic Program for Intro
+// Coding a welcome message using your LIN
+public class WelcomeClass{
+    public static void main(String[]args){
+
+System.out.println("   -----------");
+System.out.println("   | WELCOME |");
+System.out.println("   -----------");
+System.out.println(" ^  ^  ^  ^  ^  ^");
+System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("<-R--W--A--2--1--8->");
+System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println(" v  v  v  v  v  v ");
+System.out.println("Hi, my name is Riley and I like to eat steak");
+}
+}
